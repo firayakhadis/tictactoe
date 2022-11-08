036ed69a255dd560bf0c9624f4f3243cd61b9f79
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-11-08 19:10:08 +0300

    tictactoe

--- main.py
+++ main.py
@@ -33,7 +33,18 @@ def playerMove():
     run = True
     while run:
         move = input('Please select a position to place an \'X\' (1-9): ')
-
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Soory, this space is occupied!')
+            else:
+                print('Please type a number within the range!')
+        except:
+            print('Please type a number!')
 
 def compMove():
     pass
